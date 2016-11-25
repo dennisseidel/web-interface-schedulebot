555cb515f215496dfdbaa2a46b7d41a7707c096f
Dennis Seidel <mail@dennisseidel.de>
2016-11-25 17:39:28 +0100

    feat: create a redux store with a middleware
    http://redux.js.org/docs/basics/Store.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 
 // Create new component. That componetn should produce HTML.
 import App from './components/app.js';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // Put components into HTML
 ReactDOM.render(<App />, document.querySelector('.container'));
