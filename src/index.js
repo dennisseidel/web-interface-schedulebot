732303e21e1fc88a7275af3ef25138556c06202e
Dennis Seidel <mail@dennisseidel.de>
2016-11-25 17:45:19 +0100

    feat: create a Redux store for the application

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { createStore, applyMiddleware } from 'redux';
 
 // Create new component. That componetn should produce HTML.
 import App from './components/app.js';
+import reducers from './reducers/';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+let store = createStore(reducers);
 
 // Put components into HTML
 ReactDOM.render(<App />, document.querySelector('.container'));
