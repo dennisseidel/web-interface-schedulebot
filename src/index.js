ff65b2dabc9c1dff9848852529710c9e90dcb823
Dennis Seidel <mail@dennisseidel.de>
2016-11-25 13:00:13 +0100

    feat: render app component in DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create new component. That componetn should produce HTML.
 const App = () => {
   return <div>Hi!</div>;
 }
+// Put components into HTML
+ReactDOM.render(<App />, document.querySelector('.container'));
