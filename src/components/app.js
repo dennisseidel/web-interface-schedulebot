bc86e91c4a5cb92a71d5502ed6bd7a6d9d7b7467
Dennis Seidel <mail@dennisseidel.de>
2016-11-25 16:14:11 +0100

    feat: create booklist component
    this booklist component take takes the application state and renders it

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 // Create new component. That componetn should produce HTML.
 import React, { Component } from 'react';
 
+import BookList from "./book-list.js";
+
 class App extends Component {
   render() {
     return (
-      <div>Bookshop</div>
+      <div>
+        <h1>Bookshop</h1>
+        <BookList />
+      </div>
     );
   }
 }
