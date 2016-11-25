070fafb28db8112a54f81d27a1a7d32f43a3e2c6
Dennis Seidel <mail@dennisseidel.de>
2016-11-25 15:46:05 +0100

    refactor: extract app component in seperate file
    also make it a class component

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+// Create new component. That componetn should produce HTML.
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Bookshop</div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // Create new component. That componetn should produce HTML.
-const App = () => {
-  return <div>Hi!</div>;
-}
+import App from './components/app.js';
+
 // Put components into HTML
 ReactDOM.render(<App />, document.querySelector('.container'));
