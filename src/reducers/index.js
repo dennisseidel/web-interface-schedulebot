65a657333766ec971b6f3f9fcc22baa3b88d1cd0
Dennis Seidel <mail@dennisseidel.de>
2016-11-26 09:09:06 +0100

    feat: combine the ActiveBook reducer into the rootReducer > app state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import ReducerBooks from './reducer_books.js';
+import ActiveBook from './reducer_active_book.js';
 
 const rootReducer = combineReducers({
-  books: ReducerBooks
+  books: ReducerBooks,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
