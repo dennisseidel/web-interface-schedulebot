86904b830fdf70b7d20f39f0c9c96bb912dcad47
Dennis Seidel <mail@dennisseidel.de>
2016-11-26 01:08:46 +0100

    fix: change "import" to "from"

--- src/components/book-list.js
+++ src/components/book-list.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { selectBook } import "../actions/index.js";
+import { selectBook } from "../actions/index.js";
 import { bindActionCreators } from "redux";
 
 class BookList extends Component {
