3ae2f12c9e7559d359583c52d7df047b2a106547
Dennis Seidel <mail@dennisseidel.de>
2016-12-20 12:30:58 +0100

    feat: route to multiple routes with react router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHostory } from 'react-router';
 
 // Create new component. That componetn should produce HTML.
 import App from './components/app.js';
@@ -11,7 +12,10 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // Put components into HTML
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHostory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>,
   document.querySelector('.container'));
