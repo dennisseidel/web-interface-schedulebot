259dc065795e31b7d479d0f6aabae241b60032e6
Dennis Seidel <mail@dennisseidel.de>
2016-12-20 13:03:26 +0100

    feat: sign the user in
    for this we use redux form that provides use with a reducer that create
    the global state from the form.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer } from 'redux-form';
+
 import ReducerBooks from './reducer_books.js';
 import ActiveBook from './reducer_active_book.js';
 
 const rootReducer = combineReducers({
+  form: reducer,
+  
   books: ReducerBooks,
   activeBook: ActiveBook
 });
