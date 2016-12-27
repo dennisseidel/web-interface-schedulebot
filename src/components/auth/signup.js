0bfc2f5adf283ba5341045777d72f27a9d2ed6fc
Dennis Seidel <mail@dennisseidel.de>
2016-12-27 02:58:56 +0100

    feat: scaffold the signup form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -6,10 +6,36 @@ import * as actions from '../../actions/';
 
 class SignupForm extends Component {
   render() {
+    const { handleSubmit } = this.props;
     return(
-      <div>Signup</div>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" className="form-control"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <Field name="password" component="input" type="password" className="form-control"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordConfirm">Confirm Password</label>
+          <Field name="passwordConfirm" component="input" type="password" className="form-control"/>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </form>
     );
   }
 }
+
+// Decorate the form component
+SignupForm = reduxForm({
+  // a unique name for this form
+  form: 'signup'
+})(SignupForm);
+
+//Use connect from react-redux to gain access to actions
+SignupForm = connect(null, actions)(SignupForm);
 
 export default SignupForm;
