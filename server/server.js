da5674806520bdb071ba5065f52f608affdf9042
Dennis Seidel <mail@dennisseidel.de>
2017-02-04 15:34:54 +0100

    chore: add production config

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const compress = require('compression');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Apply gzip compression
+app.use(compress());
+
+// Serve static assets from ~/public since Webpack is unaware of
+// these files. This middleware doesn't need to be enabled outside
+// of development since this directory will be copied into ~/dist
+// when the application is compiled.
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', (err) => {
+  if (err) {
+    console.log(`Error: ${err}`);
+  }
+});
---
+++ webpack.production.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: `${__dirname}/public`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        //Include accepts either a path or an array of paths
+        //include: PATHS.app
+      },
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      }],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+  },
+  // devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new CopyWebpackPlugin([{
+      from: './*.html',
+    }]),
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+  ],
+};
