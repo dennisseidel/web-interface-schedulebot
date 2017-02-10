1bbfdd88c2ceb08d4c8f884cf6240c8b868e92be
Dennis Seidel <mail@dennisseidel.de>
2017-02-10 16:30:58 +0100

    chore: migrate prod config to webpack2

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,37 +3,50 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/index.js',
-  ],
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'public'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   module: {
-    loaders: [
-      {
-        // Test expects a RegExp! Note the slashes!
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        //Include accepts either a path or an array of paths
-        //include: PATHS.app
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: [
+        'babel-loader',
+      ],
+      query: {
+        presets: ['react', 'es2015', 'stage-1'],
       },
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1'],
-        },
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
-      }],
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+      ],
+    },
+    ],
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
+  resolve:
+  {
+    extensions: ['', '.json', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.join(__dirname, './src'),
+    ],
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
   },
   devServer: {
     historyApiFallback: true,
