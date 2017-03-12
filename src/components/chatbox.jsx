623f7898aabd5e5b7016cdd2570e0a281c05c1b4
Dennis Seidel <mail@dennisseidel.de>
2017-03-12 15:54:54 +0100

    feat: allow the server to send html
    better send a data structure with links ...
    and then render them differently based on the context ...

--- src/components/chatbox.jsx
+++ src/components/chatbox.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import '../../style/chatbox.scss';
 
+function createMarkup(text) {
+  return { __html: text };
+}
+
 const ChatBox = props => (
   <div className={`div${props.usertype} row ${(props.usertype === 'bot') ? 'justify-content-start' : 'justify-content-end'}`} >
     <div className="col-8">
-      <span className={`span${props.usertype}`}>
-        {props.text}
-      </span>
+      <span className={`span${props.usertype}`} dangerouslySetInnerHTML={createMarkup(props.text)} />
     </div>
   </div>
   );
